417d0a168503b4cd3be57b9b9be52746ab4c5d6e
James Carter <jamess.carter41@gmail.com>
2025-12-18 04:27:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function generateCPF() {
+  let n = Array.from({length:9}, () => Math.floor(Math.random()*10));
+  let d1 = (n[0]*10 + n[1]*9 + n[2]*8 + n[3]*7 + n[4]*6 + n[5]*5 + n[6]*4 + n[7]*3 + n[8]*2) % 11;
+  d1 = (d1 < 2) ? 0 : 11 - d1;
+  let d2 = (n[0]*11 + n[1]*10 + n[2]*9 + n[3]*8 + n[4]*7 + n[5]*6 + n[6]*5 + n[7]*4 + n[8]*3 + d1*2) % 11;
+  d2 = (d2 < 2) ? 0 : 11 - d2;
+  document.getElementById('result').innerText = n.join('') + d1 + d2;
+}
 
